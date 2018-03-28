a45f841ef63cd17f95872a85f63349560827e98e
Jesse A. Tov <jesse.tov@gmail.com>
2018-03-28 00:42:22 -0500

    Renamed bit_slice module to slice.

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@ pub use self::storage::BlockType;
 mod traits;
 pub use self::traits::{BitVec, BitVecMut, BitVecPush};
 
-mod bit_slice;
-pub use self::bit_slice::{BitSlice, BitSliceMut};
+mod slice;
+pub use self::slice::{BitSlice, BitSliceMut};
 
 mod prims;
 
