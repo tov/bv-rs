8455a6594bb91ea0257647359ef2fe2f7a4f6d6f
Jesse A. Tov <jesse.tov@gmail.com>
2018-06-15 03:32:27 -0500

    Added slice benches.

--- benches/bit_vec.rs
+++ benches/bit_vec.rs
@@ -3,7 +3,7 @@
 extern crate bv;
 extern crate test;
 
-use bv::{BitVec, BitsMut};
+use bv::{Bits, BitsMut, BitVec};
 use test::Bencher;
 
 const NBITS: u64 = 640;
@@ -48,6 +48,53 @@ fn bit_vec_read_write(b: &mut Bencher) {
         for i in 0 .. bv.len() {
             let bit = bv[i] ^ get_bit();
             bv.set_bit(i, bit);
+        }
+    })
+}
+
+#[bench]
+fn slice_read(b: &mut Bencher) {
+    let v = vec![0u64; NBITS as usize / 64];
+    let slice = v.as_slice();
+
+    b.iter(|| {
+        let mut result = false;
+
+        for i in 0 .. slice.bit_len() {
+            result ^= slice.get_bit(i as u64);
+        }
+
+        result
+    })
+}
+
+#[bench]
+fn slice_write(b: &mut Bencher) {
+    let mut v = vec![0u64; NBITS as usize / 64];
+    let slice = v.as_mut_slice();
+
+    #[inline(never)]
+    fn get_bit() -> bool { false }
+
+    b.iter(|| {
+        for i in 0 .. slice.bit_len() {
+            slice.set_bit(i as u64, get_bit());
+        }
+    })
+}
+
+#[bench]
+fn slice_read_write(b: &mut Bencher) {
+    let mut v = vec![0u64; NBITS as usize / 64];
+    let slice = v.as_mut_slice();
+
+    #[inline(never)]
+    fn get_bit() -> bool { false }
+
+    b.iter(|| {
+        for i in 0 .. slice.bit_len() {
+            let bit = slice.get_bit(i) ^ get_bit();
+            slice.set_bit(i, bit);
         }
     })
 }
