0807fc63b1848ccabb57b5b452ed2a88e3421cb0
Jesse A. Tov <jesse.tov@gmail.com>
2018-06-18 15:26:04 -0500

    Improved benches.

--- benches/to_bit_vec.rs
+++ benches/to_bit_vec.rs
@@ -9,20 +9,26 @@ use test::Bencher;
 const NBLOCKS: usize = 300;
 
 #[bench]
-fn copy_slice_array(b: &mut Bencher) {
+fn to_bit_vec_vec(b: &mut Bencher) {
+    let vec = vec![0, NBLOCKS];
+    b.iter(|| vec.to_bit_vec());
+}
+
+#[bench]
+fn to_bit_vec_array(b: &mut Bencher) {
     let vec = vec![0, NBLOCKS];
     let slice = vec.as_slice();
     b.iter(|| slice.to_bit_vec());
 }
 
 #[bench]
-fn copy_slice_bit_vec(b: &mut Bencher) {
+fn to_bit_vec_bit_vec(b: &mut Bencher) {
     let vec: BitVec = bit_vec![false; usize::mul_nbits(NBLOCKS)];
     b.iter(|| vec.to_bit_vec());
 }
 
 #[bench]
-fn copy_slice_bit_slice(b: &mut Bencher) {
+fn to_bit_vec_bit_slice(b: &mut Bencher) {
     let vec: BitVec = bit_vec![false; usize::mul_nbits(NBLOCKS)];
     let slice = vec.bit_slice(..);
     b.iter(|| slice.to_bit_vec());
